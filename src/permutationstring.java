b053e1c84dbd066d21c221620a6782226701c40d
SakthivelmariArumugam <sakthivelmaribe@gmail.com>
2024-10-11 14:53:29 +0530

    this soution is correct but not optimal

--- src/permutationstring.java
+++ src/permutationstring.java
@@ -1,26 +1,48 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class permutationstring{
     public static boolean fun(String s1,String s2)
     {
         ArrayList<String> list1=new ArrayList<>();
-        for(int i=0;i<s2.length()-1;i++)
-        {
-            String m1="";
-            String m2=String.valueOf(s2.charAt(i));
-            String m3=String.valueOf(s2.charAt(i+1));
-            m1=m2+m3;
-            list1.add(m1);
-        }
-        for(int i=0;i<list1.size();i++)
-        {
-            for(int j=0;j<s1.length();j++)
-            {
-                System.out.println(list1.get(i));
-            }
-        }
-        return false;
+       for(int i=0;i<s2.length();i++)
+       {
+           int l=i+s1.length();
+           String sum="";
+           if(l<s2.length()+1)
+           {
+               for(int j=i;j<l;j++)
+               {
+
+                   String k=String.valueOf(s2.charAt(j));
+                   sum=sum+k;
+
+               }
+               char b[]=sum.toCharArray();
+               Arrays.sort(b);
+               sum=String.valueOf(b);
+               //System.out.println(String.valueOf(sum));
+           }
+           list1.add(sum);
+       }
+       String sum1="";
+       for(int i=0;i<s1.length();i++)
+       {
+
+           sum1=sum1+String.valueOf(s1.charAt(i));
+       }
+       char b[]=sum1.toCharArray();
+       Arrays.sort(b);
+      sum1=String.valueOf(b);
+      // System.out.println(sum1);
+       if(list1.contains(sum1))
+       {
+           return true;
+       }
+       return false;
+
     }
     public static void main(String[] args)
     {
@@ -28,6 +50,7 @@ public class permutationstring{
         String s1=sc.nextLine();
         String s2=sc.nextLine();
         boolean result=fun(s1,s2);
+        System.out.println(result);
 
     }
 }
